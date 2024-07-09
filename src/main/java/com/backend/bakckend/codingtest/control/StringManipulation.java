db45f64a22480d29bae142d21eab13859f3eece4
iamipro <iamipro@naver.com>
2024-07-09 22:04:15 +0900

    문자열조작

--- src/main/java/com/backend/bakckend/codingtest/control/StringManipulation.java
+++ src/main/java/com/backend/bakckend/codingtest/control/StringManipulation.java
@@ -4,45 +4,6 @@ import java.util.Arrays;
 
 public class StringManipulation {
 
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-        while(left < right){
-            if(str.charAt(left) != str.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    public static String compressString(String str){
-        StringBuilder sb = new StringBuilder();
-        int count =1;
-        for(int i = 0; i < str.length(); i++){
-            if(i == str.length()-1 || str.charAt(i) != str.charAt(i+1)){
-                sb.append(str.charAt(i));
-                sb.append(count);
-                count = 1;
-            }else{
-                count++;
-            }
-        }
-        return sb.toString();
-    }
-
-    public static boolean isAnagram(String str1, String str2){
-        if(str1.length() != str2.length()){
-            return false;
-        }
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1, arr2);
-    }
-
-
 
     public static void main(String[] args) {
 
@@ -72,9 +33,62 @@ public class StringManipulation {
         String compressed = compressString(str6);
         System.out.println("압축된 문자열: " + compressed);
 
-
-
+        String[] strs = {"flower", "flow", "flight"};
+        String longestCommonPrefix = longestCommonPrefix(strs);
+        System.out.println("가장 긴 공통 접두사: " + longestCommonPrefix);
 
 
     }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static boolean isAnagram(String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static String longestCommonPrefix(String[] str){
+        if(str == null || str.length == 0) return "";
+        String prefix = str[0];
+        for(int i =1 ; i< str.length ; i++){
+            while(str[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    public static String compressString(String str){
+        StringBuilder sb = new StringBuilder();
+        int count =1;
+        for(int i = 0; i < str.length(); i++){
+            if(i == str.length()-1 || str.charAt(i) != str.charAt(i+1)){
+                sb.append(str.charAt(i));
+                sb.append(count);
+                count = 1;
+            }else{
+                count++;
+            }
+        }
+        return sb.toString();
+    }
+
+
 }
