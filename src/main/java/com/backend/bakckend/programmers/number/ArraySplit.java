ac1312885014e320e8e88e17953d995eaab64e6b
iamipro <iamipro@naver.com>
2024-07-26 16:26:07 +0900

    배열자르기

--- src/main/java/com/backend/bakckend/programmers/number/ArraySplit.java
+++ src/main/java/com/backend/bakckend/programmers/number/ArraySplit.java
@@ -8,8 +8,6 @@ public class ArraySplit {
         int[] answer = new int[(int) (right - left + 1)];
         int idx =0;
         for(long i =left; i<=right; i++){
-            System.out.println("i/n : " + i/n);
-            System.out.println("i%n : " + i%n);
             answer[idx] = (int) Math.max(i/n, i%n) + 1;
             idx++;
         }
