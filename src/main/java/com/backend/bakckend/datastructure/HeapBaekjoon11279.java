3ca2a0260752e82d4f56d2623990885f0ea131eb
iamipro <iamipro@naver.com>
2024-08-01 11:00:43 +0900

    rf

--- src/main/java/com/backend/bakckend/datastructure/HeapBaekjoon11279.java
+++ src/main/java/com/backend/bakckend/datastructure/HeapBaekjoon11279.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class Main {
+public class HeapBaekjoon11279 {
 
 
     public static void main(String[] args) {
